8375e374fcc85219009495ebd3b53efcac875eb1
Alejandro Arroyo <alejandro.adesis@gmail.com>
2014-10-23 17:28:48 +0200

    feat(fisicas): player con fisicas

--- src/js/ejemplos/ejemplo1.js
+++ src/js/ejemplos/ejemplo1.js
@@ -9,9 +9,9 @@
                 y = this.game.height / 2;
 
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            this.game.physics.arcade.gravity.y = 250;
 
             this.bg = this.game.add.tileSprite(0, 0, 640, 480, 'sky');
-            // this.bg = this.game.add.tileSprite(x, y, ancho, alto, 'clave');
             this.bg.fixedToCamera = true;
 
             // map
@@ -23,20 +23,25 @@
 
             function setupPlayer(player) {
                 player.anchor.setTo(0.5, 0.5);
+                player.body.collideWorldBounds = true;
                 player.animations.add('standby', [0, 1, 2, 1], 6, true);
                 player.animations.add('move', [3, 4, 5, 6, 7, 8, 9, 10, 11], 18, true);
                 player.animations.add('attack', [12, 13, 14, 15], 12, true);
                 player.animations.add('jump', [16, 17, 18, 19], 12, true);
-                // player.animations.add('nombreAnimacion', [frames], vel, loop);
                 player.scale.setTo(2, 2);
+                player.body.setSize(55, 50);
             }
 
             this.player = this.game.add.sprite(x, y, 'marco');
+            this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
             setupPlayer(this.player);
-            this.player.animations.play('attack');
+            this.player.animations.play('standby');
+            this.game.camera.follow(this.player);
         },
 
-        update: function () {}
+        update: function () {
+            game.physics.arcade.collide(this.player, this.layer);
+        }
     };
 
     window['intro'] = window['intro'] || {};
